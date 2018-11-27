ffb37b3bc63979653031ae59724943ebc08c74a5
ATRAPA\raustral <raustral@deloitte.com>
2018-11-27 16:32:09 +0800

    day 1

---
+++ force-app/main/default/aura/customerTile/customerTileHelper.js
@@ -0,0 +1,30 @@
+({
+    sendSobjectEvent : function(recId, sobject) 
+    {
+        var selectEvt = $A.get("e.ltng:selectSObject"); 
+        
+        selectEvt.setParams({
+            "recordId": recId, 
+            "channel": sobject 
+        }); 
+        
+        selectEvt.fire(); 
+        //Send data to c:reservationHelper
+    },
+    
+    // IGNORE ME FOR NOW
+    sendMuteEvent : function(recId)
+    {
+        var mutes = new Set();
+        mutes.add(recId);
+        console.log('mutes set: ', mutes);
+        var action = $A.get("e.c:muteTiles");
+        
+        action.setParams({
+            "muteArray" : mutes,
+            "channel": "reservationManager"
+        });
+        
+        action.fire(); 
+    }
+})
